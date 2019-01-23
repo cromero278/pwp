49185496bdb83bb799b0c37c0b185e73cb46da0a
Cassandra Romero <romero.cassandra1@gmail.com>
2019-01-22 18:55:44 -0700

    content to milestone 1

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Milestone 1 - Personal Website Project</title>
+	</head>
+	<body>
+			<h1>PWP Milestone 1</h1>
+			<h2>Purpose, Audience and Goal</h2>
+			<h3>Purpose</h3>
+		<p>This website will be a portfolio focusing on digital media and front-end web development.</p>
+			<h3>Audience</h3>
+		<p>This website is designed to attract the interest of tech and digital media professionals nationwide, this is including
+			but not limited to hiring managers, small business owners, and recruiters. </p>
+			<h3>Goal</h3>
+		<p>The main goal of this website it to attract professionals in the tech and web development community interested
+		in collaboration and/or hiring for freelance opportunities. Additionally, this website will serve as a portfolio showcasing previous
+		work and accomplishments.</p>
+			<h2>Persona</h2>
+	</body>
+</html>
