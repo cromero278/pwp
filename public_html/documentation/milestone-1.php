47ddad28c173a8fb3bc6298fb37f996bb3e162b8
Cassandra Romero <45883869+cromero278@users.noreply.github.com>
2019-01-29 09:13:27 -0700

    Update milestone-1.php
    tabs

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -2,46 +2,46 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title>Milestone 1 - Personal Website Project</title>
+			<title>Milestone 1 - Personal Website Project</title>
 	</head>
 	<body>
 			<h1>PWP Milestone 1</h1>
 			<h2>Purpose, Audience, and Goal</h2>
 			<h3>Purpose</h3>
-		<p>This website will be a portfolio focusing on digital media and front-end web development.</p>
+				<p>This website will be a portfolio focusing on digital media and front-end web development.</p>
 			<h3>Audience</h3>
-		<p>This website is designed to attract the interest of tech and digital media professionals nationwide, this is including
+				<p>This website is designed to attract the interest of tech and digital media professionals nationwide, this is including
 			but not limited to hiring managers, small business owners, and recruiters. </p>
 			<h3>Goal</h3>
-		<p>The main goal of this website it to attract professionals in the tech and web development community interested
-		in collaboration and/or hiring for freelance opportunities. Additionally, this website will serve as a portfolio showcasing previous
-		work and accomplishments.</p>
+				<p>The main goal of this website it to attract professionals in the tech and web development community interested
+				in collaboration and/or hiring for freelance opportunities. Additionally, this website will serve as a portfolio showcasing previous
+				work and accomplishments.</p>
 			<h2>Persona</h2> <img src="emma-hoffman.jpg" alt="Emma Hoffman Photo" style="width:500px;height:300px;"><br>
 			<strong>Name:</strong> Emma Hoffman
 			<br>
-		 	<strong>Age: 32</strong>
+		 		<strong>Age: 32</strong>
 			<br>
-			<strong>Profession:</strong> Graphic Designer at Ping Identity, Denver Colorado
+				<strong>Profession:</strong> Graphic Designer at Ping Identity, Denver Colorado
 			<br>
-			<strong>Hobbies:</strong> Enjoys yoga, traveling, maintaining her lifestyle and food blog.
+				<strong>Hobbies:</strong> Enjoys yoga, traveling, maintaining her lifestyle and food blog.
 			<br>
-			<strong>Technology</strong> 2018 15 in MacBook Pro, iPhone 8
+				<strong>Technology</strong> 2018 15 in MacBook Pro, iPhone 8
 			<br>
-			<strong>Frequency of Use:</strong> Very frequent user of technology, professionally, is adept in Adobe Creative Suite. Personally uses multiple social media networks and maintains a lifestyle blog.
+				<strong>Frequency of Use:</strong> Very frequent user of technology, professionally, is adept in Adobe Creative Suite. Personally uses multiple social media networks and maintains a lifestyle blog.
 			<br>
-		    <strong>Frustrations and Needs:</strong> While Emma works with technology on a daily basis in her professional life, her blog has also become popular and potentially lucrative however
-			needs to be maintained by someone with adequate front end development and SEO experience. Emma does not have a large budget to spend on hiring a developer so is looking for a freelance developer
-			that is affordable but still effective She is overwhelmed by potential candidates she has seen on sites such as LinkedIn and Upwork.
+		    		<strong>Frustrations and Needs:</strong> While Emma works with technology on a daily basis in her professional life, her blog has also become popular and potentially lucrative however
+				needs to be maintained by someone with adequate front end development and SEO experience. Emma does not have a large budget to spend on hiring a developer so is looking for a freelance developer
+				that is affordable but still effective She is overwhelmed by potential candidates she has seen on sites such as LinkedIn and Upwork.
 			<br>
-			<strong>Attitudes and Behavior:</strong> Because Emma has a background in graphic design she is extremely particular about those she chooses to work with. Finding someone who is easy to work with,
-			and who can understand and interpret her "style and desired aesthetic" is important. Because she does not have much time to look for candidates since this is a personal project, she wants to quickly sift through personal
-			portfolios.
+				<strong>Attitudes and Behavior:</strong> Because Emma has a background in graphic design she is extremely particular about those she chooses to work with. Finding someone who is easy to work with,
+				and who can understand and interpret her "style and desired aesthetic" is important. Because she does not have much time to look for candidates since this is a personal project, she wants to quickly sift through personal
+				portfolios.
 			<br>
-			<strong>Goals</strong>
-			Emma's overall goal is to find an affordable web developer to optimize her website and assist with SEO. Emma is working on a tight budget and but still wants a developer who can is creative and can create
-			her vision. She is using personal portfolio websites to get a feel for individual style and experience.
+				<strong>Goals</strong>
+				Emma's overall goal is to find an affordable web developer to optimize her website and assist with SEO. Emma is working on a tight budget and but still wants a developer who can is creative and can create
+				her vision. She is using personal portfolio websites to get a feel for individual style and experience.
 			<br>
-			<strong>User Story</strong>
-			As a user, I need to view personal portfolio websites to determine whether I should pursue hiring this person for a freelance web development job.  <br>
+				<strong>User Story</strong>
+				As a user, I need to view personal portfolio websites to determine whether I should pursue hiring this person for a freelance web development job.  <br>
 	</body>
 </html>
