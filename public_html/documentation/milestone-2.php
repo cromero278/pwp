cc12e3392260a014888c3f4777006004e6e208c0
Cassandra Romero <romero.cassandra1@gmail.com>
2019-02-01 15:02:36 -0700

    milestone 2 content

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -9,13 +9,18 @@
 		<p>
 			<ul>
 				<li>
-				Welcome:
+				Intro: My name and a few words about who I am as a professional. This text will lay over a background image of something
+				tech/design related.
 				</li>
 				<li>
-				About:
+				About me: This section will include several paragraph about my experience in web development and multimedia. Additionally
+				this section will include a quick blurb about my hobbies outside of work. There will be a headshot/professional photo
+					aligned left in this section.
 				</li>
 				<li>
-				Portfolio:
+				Portfolio: This section will include several examples of projects I have completed in the multimedia and/or
+				web development. The section will include photos from the project and under each photo there will be a brief description
+				of the project.
 				</li>
 				<li>
 				Contact:
