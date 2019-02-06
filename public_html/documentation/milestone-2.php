55e6129fcee78e928d51419b55cb47640888c1cb
Cassandra Romero <romero.cassandra1@gmail.com>
2019-02-06 13:19:56 -0700

    code clean up

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -7,27 +7,26 @@
 	<body>
 		<h1>Milestone 2</h1>
 		<h2>Content Strategy</h2>
-		<p>
-			<ul>
-				<li>
-				<strong>Intro:</strong> My name and a few words about who I am as a professional. This text will lay over a background image of something
-				tech/design related.
-				</li>
-				<li>
-				<strong>About me:</strong> This section will include several paragraph about my experience in web development and multimedia. Additionally
-				this section will include a quick blurb about my hobbies outside of work. There will be a headshot/professional photo
-					aligned left in this section.
-				</li>
-				<li>
-				<strong>Portfolio:</strong> This section will include several examples of projects I have completed in the multimedia and/or
-				web development. The section will include photos from the project and under each photo there will be a brief description
-				of the project.
-				</li>
-				<li>
-				<strong>Contact:</strong> This section will include a secure contact form. It will include forms for first and last name, email address
-				and a form to input additional comments or questions.
-				</li>
-			</ul>
+		<ul>
+			<li>
+			<strong>Intro:</strong> My name and a few words about who I am as a professional. This text will lay over a background image of something
+			tech/design related.
+			</li>
+			<li>
+			<strong>About me:</strong> This section will include several paragraph about my experience in web development and multimedia. Additionally
+			this section will include a quick blurb about my hobbies outside of work. There will be a headshot/professional photo
+			aligned left in this section.
+			</li>
+			<li>
+			<strong>Portfolio:</strong> This section will include several examples of projects I have completed in the multimedia and/or
+			web development. The section will include photos from the project and under each photo there will be a brief description
+			of the project.
+			</li>
+			<li>
+			<strong>Contact:</strong> This section will include a secure contact form. It will include forms for first and last name, email address
+			and a form to input additional comments or questions.
+			</li>
+		</ul>
 		<h2>Wireframes</h2>
 		<p>These wireframes reflect a mobile-first, one page website, made using Balsamiq.</p>
 		<img src="desktop-wireframe2.0.png" alt="cassandra romero desktop wireframe" style="height: 600px">
