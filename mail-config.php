b35594189612d2743ffe52ce04921c034074ec0d
Cassandra Romero <romero.cassandra1@gmail.com>
2019-02-23 12:26:34 -0700

    contact form php and json files added

---
+++ mail-config.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * mail-config.php
+ * This file contains your reCAPTCHA API keys and your recipient's email addresses.
+ *
+ * @param string $siteKey your public reCAPTCHA API key
+ * @param string $secret your secret reCAPTCHA API key
+ * @param array $MAIL_RECIPIENTS array of email addresses and corresponding recipient names to send form responses to
+ *
+ * @author Rochelle Lewis <rlewis37@cnm.edu>
+ *
+ * This file contains sensitive information and should ALWAYS be gitignored!
+ **/
+
+//the SendGrid SMTP authorization
+$smtpUser = '-- SendGrid USER --';
+$smtpSecret = '-- SendGrid SECRET --';
+
+// your Google reCAPTCHA keys here
+$siteKey = "6LfFdpMUAAAAAKXe_UeqRN6Ybznfi7jMNN-43z6c";
+;
+$secret = "6LfFdpMUAAAAACC8lEZZITWUzM_BB08aGs8u4iOg
+";
+
+/**
+ * attach the recipients to the message
+ * notice this an array that can include or omit the the recipient's real name
+ * use the recipients' real name where possible; this reduces the probability of the Email being marked as spam
+ **/
+$MAIL_RECIPIENTS = ["mail@email.com" => "Recipient Name"];
