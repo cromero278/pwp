4d0c436ef11f08f2d67cbf232c2ce3403acf7fda
Cassandra Romero <romero.cassandra1@gmail.com>
2019-02-26 13:30:20 -0700

    mailer edits

--- public_html/php/mailer.php
+++ public_html/php/mailer.php
@@ -1,7 +1,63 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Pure-Manage
- * Date: 2/25/2019
- * Time: 10:24 AM
- */
+ * mailer.php
+ *
+ * This file handles secure mail transport using the Swiftmailer
+ * library with Google reCAPTCHA integration.
+ *
+ * @author Rochelle Lewis <rlewis37@cnm.edu>
+ **/
+// require all composer dependencies
+require_once(dirname(__DIR__, 2) . "/vendor/autoload.php");
+// require mail-config.php
+require_once("mail-config.php");
+
+use \SendGrid\Mail;
+
+$sendgrid = new \SendGrid($smtpSecret);
+// verify user's reCAPTCHA input
+$recaptcha = new \ReCaptcha\ReCaptcha($secret);
+
+$resp = $recaptcha->verify($_POST["g-recaptcha-response"], $_SERVER["REMOTE_ADDR"]);
+try {
+	// if there's a reCAPTCHA error, throw an exception
+//	if (!$resp->isSuccess()) {
+//		throw(new Exception("reCAPTCHA error!"));
+//	}
+	/**
+	 * Sanitize the inputs from the form: name, email, subject, and message.
+	 * This assumes jQuery (NOT Angular!) will be AJAX submitting the form,
+	 * so we're using the $_POST superglobal.
+	 **/
+	$name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+	$subject = filter_input(INPUT_POST, "subject", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$message = filter_input(INPUT_POST, "text", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	// create SendGrid object
+	$emailObject = new \SendGrid\Mail\Mail();
+	/**
+	 * Attach the sender to the message.
+	 * This takes the form of an associative array where $email is the key for the real name.
+	 **/
+	$emailObject->setFrom($email, $name);
+	/**
+	 * Attach the recipients to the message.
+	 * $MAIL_RECIPIENTS is set in mail-config.php
+	 **/
+	$recipients = $MAIL_RECIPIENTS;
+	$emailObject->addTo($recipients[0], $recipients[1]);
+	// attach the subject line to the message
+	$emailObject->setSubject("Inquiry");
+	/**
+	 * Attach the actual content for the email.
+	 **/
+	$emailObject->addContent("text/plain", $message);
+	/**
+	 * using the sendgrid object from above call the send method and use the emailObject as an argument.
+	 */
+	$response = $sendgrid->send($emailObject);
+	// report a successful send!
+	echo "<div class=\"alert alert-success\" role=\"alert\">Success!</div>";
+} catch(\Exception $exception) {
+	echo "<div class=\"alert alert-danger\" role=\"alert\"><strong>Oh no!</strong>Your email was unable to send" . $exception->getMessage() . "</div>";
+}
