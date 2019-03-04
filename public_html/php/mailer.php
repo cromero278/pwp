57e10cd053389963165f8867db614f1996e09dee
Cassandra Romero <romero.cassandra1@gmail.com>
2019-03-03 19:33:02 -0700

    added portfolio content

--- public_html/php/mailer.php
+++ public_html/php/mailer.php
@@ -21,9 +21,9 @@ $recaptcha = new \ReCaptcha\ReCaptcha($secret);
 $resp = $recaptcha->verify($_POST["g-recaptcha-response"], $_SERVER["REMOTE_ADDR"]);
 try {
 	// if there's a reCAPTCHA error, throw an exception
-//	if (!$resp->isSuccess()) {
-//		throw(new Exception("reCAPTCHA error!"));
-//	}
+if (!$resp->isSuccess()) {
+	throw(new Exception("reCAPTCHA error!"));
+	}
 	/**
 	 * Sanitize the inputs from the form: name, email, subject, and message.
 	 * This assumes jQuery (NOT Angular!) will be AJAX submitting the form,
@@ -59,5 +59,5 @@ try {
 	// report a successful send!
 	echo "<div class=\"alert alert-success\" role=\"alert\">Success!</div>";
 } catch(\Exception $exception) {
-	echo "<div class=\"alert alert-danger\" role=\"alert\"><strong>Oh no!</strong>Your email was unable to send: " . $exception->getMessage() . "</div>";
+	echo "<div class=\"alert alert-danger\" role=\"alert\"><strong>Oh no! </strong>Your email was unable to send: " . $exception->getMessage() . "</div>";
 }
